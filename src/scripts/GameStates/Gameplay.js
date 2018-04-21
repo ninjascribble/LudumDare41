b0af62998b358b7f2d1a10060955880fc31054ec
Scott Grogan <scott@ninjascript.com>
2018-04-21 09:27:16 -0700

    Adding individual bricks

--- src/scripts/GameStates/Gameplay.js
+++ src/scripts/GameStates/Gameplay.js
@@ -2,6 +2,9 @@ import DisplayObjects from '../DisplayObjects';
 
 export default class Gameplay extends Phaser.State {
   create () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 100;
+
     DisplayObjects.titleCard(game, game.width / 2, 45);
   }
 }
