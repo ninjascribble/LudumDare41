6c1a8ceffbb634a26e3a4cfc345eb39fb526aa2e
Scott Grogan <scott@ninjascript.com>
2018-04-21 12:36:03 -0700

    Adding WorldManager

--- src/scripts/DisplayObjects/Tetronimo.js
+++ src/scripts/DisplayObjects/Tetronimo.js
@@ -1,11 +1,28 @@
 import DisplayObjects from './index';
 
+const TYPES = [
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }],
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }],
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }],
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }],
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }],
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }],
+  [{ x: 0, y: 16 }, { x: 16, y: 16 }, { x: 16, y: 0 }, { x: 32, y: 16 }]
+];
+
 export default class Tetronimo extends Phaser.Group {
   constructor (game, x = 0, y = 0, type = 0) {
     super(game);
-    this.addChild(DisplayObjects.brick(game, 0, 16, 0));
-    this.addChild(DisplayObjects.brick(game, 16, 16, 0));
-    this.addChild(DisplayObjects.brick(game, 16, 0, 0));
-    this.addChild(DisplayObjects.brick(game, 32, 16, 0));
+    this.buildBricks(type, x, y);
+  }
+
+  buildBricks (type, x = 0, y = 0) {
+    const template = TYPES[type];
+    const color = type;
+
+    template.forEach((coords) => {
+      const brick = DisplayObjects.brick(game, coords.x + x, coords.y + y, color);
+      this.addChild(brick);
+    });
   }
 }
